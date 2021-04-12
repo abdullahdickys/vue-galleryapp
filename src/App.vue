cbe52cb30062b313c8a038919b61b77525ab50e6
abdullahdickys <abdullah.dicky.syarifuddin@gmail.com>
2021-04-12 15:11:32 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,96 @@
+<template>
+  <div id="app">
+  <b-container>
+  <!-- Content here -->
+<div>
+  <b-jumbotron> 
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <h3>Programmer N' Graphic Designer </h3>
+    <h5>Adobe Illustrator | Corel Draw | Laravel | Vue js</h5>
+    <b-button variant="primary" href="#">More Info</b-button>
+  </b-jumbotron>
+				</div>
+  <div>
+  <b-card-group columns>
+    <b-card
+      title="Card title that wraps to a new line"
+      img-src="https://placekitten.com/g/400/450"
+      img-alt="Image"
+      img-top
+    >
+      <b-card-text>
+        <b-button variant="outline-primary" href="https://drive.google.com/file/d/1-TstRIATEpRiX3SJ0GGxuSUceuc6ghi5/view?usp=sharing">Download</b-button>
+			</b-card-text>
+    </b-card>
+
+    <b-card header="Quote">
+      <blockquote class="blockquote mb-0">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+        <footer class="blockquote-footer">
+          Someone famous in <cite title="Source Title">Source Title</cite>
+        </footer>
+      </blockquote>
+    </b-card>
+
+    <b-card title="Title" img-src="https://placekitten.com/500/350" img-alt="Image" img-top>
+      <b-card-text>
+        This card has supporting text below as a natural lead-in to additional content.
+      </b-card-text>
+      <b-card-text class="small text-muted">Last updated 3 mins ago</b-card-text>
+    </b-card>
+
+    <b-card bg-variant="primary" text-variant="white">
+      <blockquote class="card-blockquote">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+        <footer>
+          <small>Someone famous in <cite title="Source Title">Source Title</cite></small>
+        </footer>
+      </blockquote>
+    </b-card>
+
+    <b-card>
+      <b-card-title>Title</b-card-title>
+      <b-card-text>
+        This card has supporting text below as a natural lead-in to additional content.
+      </b-card-text>
+      <b-card-text class="small text-muted">Last updated 3 mins ago</b-card-text>
+    </b-card>
+
+    <b-card img-src="https://picsum.photos/400/400/?image=41" img-alt="Image" overlay></b-card>
+
+    <b-card img-src="https://picsum.photos/400/200/?image=41" img-alt="Image" img-top>
+      <b-card-text>
+        This is a wider card with supporting text below as a natural lead-in to additional content.
+        This card has even longer content than the first.
+      </b-card-text>
+      <template #footer>
+        <small class="text-muted">Footer Text</small>
+      </template>
+    </b-card>
+  </b-card-group>
+</div>
+  </b-container>  
+  </div>
+</template>
+
+<script>
+/* import HelloWorld from './components/HelloWorld.vue' */
+
+export default {
+  name: 'App',
+  components: {
+    /* HelloWorld */
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
